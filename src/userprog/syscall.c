4a9431ac82e8c0e05a6dc986d182fd46081c9c09
kgwangrae <k.gwangrae@gmail.com>
2015-05-25 23:38:17 +0900

    Syscall draft #1 - pointer validity checks, syscall_handler and so on

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,19 +1,27 @@
 #include "userprog/syscall.h"
 #include "userprog/process.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/synch.h"
 #include "threads/malloc.h"
+#include "threads/vaddr.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
 #include "filesys/filesys.h"
 
-#define MAX_ARGS 3
-
+static void syscall_handler (struct intr_frame *);
 struct lock fs_lock;
 
-static void syscall_handler (struct intr_frame *);
+#define MAX_ARGS 3 // 3 args are enough for system calls.
+void get_arg (struct intr_frame *f, int *arg, int n);
+
+#define UADDR_BASE (const void *) 0x08084000
+int ptr_user_to_kernel(const void *vaddr);
+void ptr_validate (const void *vaddr);
+void buf_validate (const void *buf, unsigned size);
+
 
 void syscall_init (void) 
 {
@@ -25,59 +33,106 @@ void syscall_init (void)
 static void syscall_handler (struct intr_frame *f UNUSED) 
 {
   int arg[MAX_ARGS];
+  ptr_validate((const void *) f->esp);
   switch (* (int *) f->esp)
   {
+    //void halt (void)
     case SYS_HALT:
       {
         halt();
         break;
       }
+    //void exit (int status)
     case SYS_EXIT:
       {
+        get_arg(f, &arg[0], 1);
+        exit(arg[0]);
         break;
       }
+    //pid_t exec (const char *file)
     case SYS_EXEC:
       {
+        get_arg(f, &arg[0], 1);
+        arg[0] = ptr_user_to_kernel((const void *) arg[0]);
+        f->eax = exec((const char *) arg[0]);
         break;
       }
+    //int wait (pid_t pid)
     case SYS_WAIT:
       {
+        get_arg(f, &arg[0], 1);
+        f->eax = wait(arg[0]);
         break;
       }
+    //bool create (const char *file, unsigned initial_size)
     case SYS_CREATE:
       {
+        get_arg(f, &arg[0], 2);
+        arg[0] = ptr_user_to_kernel((const void *) arg[0]);
+        f->eax = create((const char *)arg[0], (unsigned) arg[1]);
         break;
       }
+    //bool remove (const char *file)
     case SYS_REMOVE:
       {
+        get_arg(f, &arg[0], 1);
+        arg[0] = ptr_user_to_kernel((const void *) arg[0]);
+        f->eax = remove((const char *) arg[0]);
         break;
       }
+    //int open (const char *file)
     case SYS_OPEN:
       {
+        get_arg(f, &arg[0], 1);
+        arg[0] = ptr_user_to_kernel((const void *) arg[0]);
+        f->eax = open((const char *) arg[0]);
         break;
       }
+    //int filesize (int fd)
     case SYS_FILESIZE:
       {
+        get_arg(f, &arg[0], 1);
+        f->eax = filesize(arg[0]);
         break;
       }
+    //int read (int fd, void *buffer, unsigned length)
     case SYS_READ:
       {
+        get_arg(f, &arg[0], 3);
+        buf_validate((const void *) arg[1], (unsigned) arg[2]);
+        arg[1] = ptr_user_to_kernel((const void *) arg[1]);
+        f->eax = read(arg[0], (void *) arg[1], (unsigned) arg[2]);
         break;
       }
+    //int write (int fd, const void *buffer, unsigned length)
     case SYS_WRITE:
       {
+        get_arg(f, &arg[0], 3);
+        buf_validate((const void *) arg[1], (unsigned) arg[2]);
+        arg[1] = ptr_user_to_kernel((const void *) arg[1]);
+        f->eax = write(arg[0], (const void *) arg[1],
+            (unsigned) arg[2]);
         break;
       }
+    //void seek (int fd, unsigned position)
     case SYS_SEEK:
       {
+        get_arg(f, &arg[0], 2);
+        seek(arg[0], (unsigned) arg[1]);
         break;
       }
+    //unsigned tell (int fd)
     case SYS_TELL:
       {
+        get_arg(f, &arg[0], 1);
+        f->eax = tell(arg[0]);
         break;
       }
+    //void close (int fd)
     case SYS_CLOSE:
       {
+        get_arg(f, &arg[0], 1);
+        close(arg[0]);
         break;
       }
   }
@@ -195,7 +250,8 @@ int filesize (int fd)
   return result;
 }
 
-int read (int fd, void *buffer, unsigned length) {
+int read (int fd, void *buffer, unsigned length) 
+{
   if (fd == STDIN_FILENO) 
   {
     uint8_t *buf = (uint8_t *) buffer; // 1byte char array
@@ -289,6 +345,7 @@ void exit (int status)
   struct thread *cur = thread_current ();
   
   cur->exit_status = status;
+  printf ("%s: exit(%d)\n", cur->name, status);
   thread_exit ();
 }
 
@@ -306,4 +363,53 @@ pid_t exec (const char *file)
 int wait (pid_t pid)
 {
   return process_wait (pid);
+}
+
+/* Operations for memory management and argument passing */
+
+/*
+ * -----------
+ * param 3  // esp + 12
+ * param 2  // esp + 8
+ * param 1  // esp + 4
+ * return addr <---- esp
+ * -----------
+ * Refer to 80x86 calling convention for details.
+ */
+void get_arg (struct intr_frame *f, int *arg, int n) 
+{
+  int i, *ptr;
+  for (i = 1; i <= n; i++)
+  {
+    ptr = (int *) f->esp + i;
+    ptr_validate((const void *) ptr);
+    arg[i] = *ptr;
+  }
+}
+
+int ptr_user_to_kernel(const void *vaddr) 
+{
+  ptr_validate(vaddr);
+  void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
+  if (ptr) return (int) ptr;
+  else exit(SYSCALL_ERROR);
+}
+
+/*
+ * Validate all user memory access attempts.
+ * PHYS_BASE > vaddr >= UADDR_BASE 
+ */
+void ptr_validate (const void *vaddr)
+{
+  if (!is_user_vaddr(vaddr) || vaddr < UADDR_BASE) exit(SYSCALL_ERROR);
+}
+
+/* Prevents buffer overflow. 
+ * Assume any buffer resides on memory continuously.
+ * TODO : Is the assumption above safe?
+ */
+void buf_validate (const void *buf, unsigned size)
+{
+  ptr_validate(buf);
+  ptr_validate(buf+size-1);
 }
